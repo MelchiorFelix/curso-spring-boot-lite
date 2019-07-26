8d5a2d9fe784166dddcbfadb61fbdb5bffab4483
Melchior <melchior.alencar@gmail.com>
2019-07-26 09:22:32 -0500

    version 26-07-2019

---
+++ gestao-festa/src/main/java/com/algaworks/festa/model/Convidado.java
@@ -0,0 +1,48 @@
+package com.algaworks.festa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SuppressWarnings("serial")
+@Entity
+public class Convidado implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	private String nome;
+
+	private Integer quantidadeAcompanhantes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+
+	
+
+}
